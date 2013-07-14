6ef0398b3fde2abef2c81cfffdefa133eb7f1438
Kirill Temnenkov <kirill@temnenkov.com>
2013-07-14 14:36:34 +0400

    есть и сравнение, но теперь нужен рефакторинг

--- src/main/java/bukvy/App.java
+++ src/main/java/bukvy/App.java
@@ -1,5 +1,8 @@
 package bukvy;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
@@ -13,6 +16,21 @@ public class App {
             for (String s : result) {
                 System.out.println(s);
             }
+
+            List<String> sorted = new ArrayList<>();
+            for (String s : result) {
+                sorted.add(s);
+            }
+            Collections.sort(sorted);
+
+            for (int i = 0; i < result.size(); ++i) {
+                if (!result.get(i).equals(sorted.get(i))) {
+                    System.out.println(MessageFormat.format("mismatch {0}!={1} line {2}",
+                            result.get(i), sorted.get(i), i));
+                    break;
+                }
+            }
+            System.out.println("Done");
         }
     }
 }
